16f9f5db73d6e928f67c51b6b4603ee9f0a28fb8
unknown <ali.muhammad@koderlab.com>
2023-08-04 17:43:53 +0500

    other script

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -22,6 +22,34 @@ function Layout(props) {
               })(window,document,'script','dataLayer','GTM-K9G79C9')`,
           }}
         />
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(scope, doc, tagName, src, objectName, newElement, firstElement) {
+        Array.isArray(scope["SwiftSalesObject"]) ?
+            scope["SwiftSalesObject"].push(objectName) :
+            (scope["SwiftSalesObject"] = [objectName]);
+        scope[objectName] =
+            scope[objectName] ||
+            function() {
+                scope[objectName].queries = scope[objectName].queries || [];
+                scope[objectName].queries.push(arguments);
+            };
+        scope[objectName].scriptInjectedAt = 1 * new Date();
+        newElement = doc.createElement(tagName);
+        newElement.setAttribute("id", "swift-sales-widget-script");
+        firstElement = doc.getElementsByTagName(tagName)[0];
+        newElement.async = 1;
+        newElement.src = src;
+        firstElement
+            ?
+            firstElement.parentNode.insertBefore(newElement, firstElement) :
+            doc.getElementsByTagName("head")[0].appendChild(newElement);
+    })(window, document, "script", "https://stg-script.swiftsales.io/swiftsales.js?v=1691150209304", "swiftSales");
+    swiftSales("Init", "426")`,
+  }}
+/>
 
     </div>
   )
