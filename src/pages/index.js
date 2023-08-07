7b881367ac34f1c4a6109e64c32c1c624e41ae3c
unknown <ali.muhammad@koderlab.com>
2023-08-07 17:08:31 +0500

    Gsap On trigger Body color Change Done

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,6 @@ import { CallToAction } from '@/components/CallToAction'
 import { Testimonials } from '@/components/Testimonials'
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
-import Script from "next/script";
 
 
 gsap.registerPlugin(ScrollTrigger);
@@ -21,10 +20,8 @@ export default function Home() {
     const sections = gsap.utils.toArray('.section');
 
     sections.forEach((section, index) => {
-      const gradient = section.getAttribute('data-gradient'); // Get the gradient data from the data-gradient attribute
-
-      section.style.backgroundImage = gradient; // Apply the gradient as the background image
-      section.style.backgroundSize = 'cover'; // Adjust this according to your needs
+      const bgColor = section.getAttribute('data-bgcolor'); // Get the gradient data from the data-bgcolor attribute
+      const bgImage = section.getAttribute('data-bgimage'); // Get the background image URL from the data-bgimage attribute
 
       gsap.from(section, {
         duration: 1,
@@ -37,10 +34,12 @@ export default function Home() {
           toggleActions: 'play none none none', // This ensures the animation plays when the section enters the viewport and is removed when it leaves
           onUpdate: (self) => {
             if (self.isActive) {
-              const bgColor = section.getAttribute('data-bgcolor'); // Get the background color from data-bgcolor
-              document.body.style.backgroundColor = bgColor; // Change the body background color
+              document.body.style.transition = 'background-color 0.5s ease'; // Add CSS transition for body background color
+              document.body.style.backgroundImage = `${bgColor}, url(${bgImage})`; // Set the body background color and image
+              document.body.style.backgroundSize = 'cover'; // Adjust this according to your needs
             }
           },
+          markers: true,
         },
       });
 
@@ -59,7 +58,18 @@ export default function Home() {
         });
       }
     });
+
+    // Cleanup function: Remove the ScrollTrigger instances when the component unmounts
+    return () => {
+      sections.forEach((section) => {
+        const trigger = ScrollTrigger.getById(section);
+        if (trigger) {
+          trigger.kill();
+        }
+      });
+    };
   }, []);
+  
 
 
 
@@ -81,26 +91,24 @@ export default function Home() {
         BgImageURL = ""
         bgColor="red"
       />
-      <div className="section" data-gradient="linear-gradient(180deg, rgba(189,32,46,1) 0%, rgba(188,30,44,1) 16%, rgba(189,30,45,1) 100%)"  data-bgcolor="blue">
+      <div className="section" data-bgcolor="linear-gradient(180deg, rgba(189,32,46,1) 0%, rgba(188,30,44,1) 16%, rgba(189,30,45,1) 100%)">
       <WhyChooseUs />
       </div>
-      <div className="section" data-gradient="linear-gradient(180deg, rgba(218,66,114,1) 0%, rgba(199,54,118,1) 100%)">
+
+      <div className="section" data-bgcolor="linear-gradient(180deg, rgba(218,66,114,1) 0%, rgba(199,54,118,1) 100%)">
       <ExploreMore
         Heading="Folks think we're rad. We're rated 4.8/5 from 100+ customer reviews."
         BtnLink="#"
         BtnText="Explore More Here"
       />
-      </div>
-
-      <div className="section" data-gradient="linear-gradient(180deg, rgba(200,55,118,1) 0%, rgba(187,47,121,1) 32%, rgba(178,42,124,1) 100%)">
       <KeyFeature/>
       </div>
 
-      <div className="section" data-gradient="linear-gradient(180deg, rgba(179,42,124,1) 0%, rgba(154,27,129,1) 29%, rgba(135,16,134,1) 55%, rgba(105,22,142,1) 100%)">
+      <div className="section"  data-bgcolor="linear-gradient(180deg, rgba(200,55,118,1) 0%, rgba(187,47,121,1) 32%, rgba(178,42,124,1) 100%)">
       <Tabs />
       </div>
 
-      <div className="section" data-gradient="linear-gradient(180deg, rgba(105,22,141,1) 0%, rgba(91,39,145,1) 29%, rgba(79,58,150,1) 55%, rgba(59,85,156,1) 100%)">
+      <div className="section"   data-bgcolor="linear-gradient(180deg, rgba(179,42,124,1) 0%, rgba(154,27,129,1) 29%, rgba(135,16,134,1) 55%, rgba(105,22,142,1) 100%)">
       <Pricing 
         ComboPack_SubHeading = "All In One"
         ComboPack_Heading = "Combo Package"
@@ -108,17 +116,17 @@ export default function Home() {
       />
       </div>
 
-      <div className="section" data-gradient="linear-gradient(180deg, rgba(59,85,156,1) 0%, rgba(49,85,159,1) 100%)">
+      <div className="section"  data-bgcolor="linear-gradient(180deg, rgba(59,85,156,1) 0%, rgba(49,85,159,1) 100%)">    
       <CallToAction 
          Heading = "Get Your Project Started Today"
          Para = "At our company, we value communication and understand the importance of open dialogue between our team and our clients. That's why we encourage you to talk to us – whether it's to discuss a new project, ask a question, or provide feedback on our services."
       />
-      </div>
-      <div className="section" data-gradient="linear-gradient(180deg, rgba(49,85,159,1) 0%, rgba(17,24,90,1) 13%, rgba(11,14,77,1) 31%, rgba(5,7,38,1) 61%, rgba(2,3,19,1) 100%)">
+  </div>
+      <div className="section" data-bgcolor="linear-gradient(180deg, rgba(49,85,159,1) 0%, rgba(17,24,90,1) 13%, rgba(11,14,77,1) 31%, rgba(5,7,38,1) 61%, rgba(2,3,19,1) 100%)"> 
       <Testimonials 
         Heading = "Testimonials"
       />
-      </div>
+     </div>
       
     </>
   )
